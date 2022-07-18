360520887d08142b625dfbb7495eda5d98907b87
EthanG78 <ethangarnier78@gmail.com>
2022-07-18 10:05:31 -0300

    Fixed issue where client was trying to initialize a named HTTPClient that did not exist

--- BlazorVideoChat/Client/Program.cs
+++ BlazorVideoChat/Client/Program.cs
@@ -25,7 +25,7 @@ namespace BlazorVideoChat.Client
             builder.Services.AddHttpClient("BlazorVideoChat.ServerAPI.Public", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorVideoChat.ServerAPI"));
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorVideoChat.ServerAPI.Private"));
 
             builder.Services.AddApiAuthorization();
 
